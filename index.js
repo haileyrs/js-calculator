c7020bc6bb594b58e16441c7ae9869eed009e7de
haileyrs <haileyscheevel@gmail.com>
2023-04-14 11:12:48 -0500

    finish all button functionality

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ let currentNum = 1;
 const displayScreen = document.querySelector('.display');
 displayScreen.innerHTML = 0
 
+const clearBtn = document.querySelector('.clear');
+const allClearBtn = document.querySelector('.all-clear');
+
 const evalBtn = document.querySelector('.equals');
 const numberBtns = document.querySelectorAll('.number');
+const pointBtn = document.querySelector('.dot');
 
 const addBtn = document.querySelector('.plus');
 const subtractBtn = document.querySelector('.minus');
 const multiplyBtn = document.querySelector('.multiply');
 const divideBtn = document.querySelector('.divide');
-const percentBtn = document.querySelector('.percent');
+const exponentBtn = document.querySelector('.exponent');
 
-addBtn.addEventListener('click', () => switchOperator('+'))
-subtractBtn.addEventListener('click', () => switchOperator('-'))
-multiplyBtn.addEventListener('click', () => switchOperator('*'))
-divideBtn.addEventListener('click', () => switchOperator('/'))
+addBtn.addEventListener('click', () => switchOperator('+'));
+subtractBtn.addEventListener('click', () => switchOperator('-'));
+multiplyBtn.addEventListener('click', () => switchOperator('*'));
+divideBtn.addEventListener('click', () => switchOperator('/'));
+exponentBtn.addEventListener('click', () => switchOperator('^'));
 
-numberBtns.forEach((b) => b.addEventListener('click', () => numberInput(b.textContent)))
+numberBtns.forEach((b) => b.addEventListener('click', () => numberInput(b.textContent)));
+pointBtn.addEventListener('click', () => (currentNum === 1 ? num1 = decimalInput(num1) : num2 = decimalInput(num2)));
 
-evalBtn.addEventListener('click', () => operate(operator, num1, num2))
+evalBtn.addEventListener('click', () => operate(operator, num1, num2));
+clearBtn.addEventListener('click', () => clearScreen(false));
+allClearBtn.addEventListener('click', () => clearScreen(true));
 
 const add = function (a, b) {
   return a + b;
@@ -40,53 +48,96 @@ const divide = function (a, b) {
   return a / b
 }
 
+const exponent = function (a, b) {
+    return Math.pow(a, b);
+  }
 
-// const sum = function (array) {
-//   return array.reduce((total, current) => total + current, 0);
-// };
-
-// const percent = function (a, b) {
-//   return Math.pow(a, b);
-// };
-
-function operate(operator, num1, num2) {
-  switch(operator) {
+function operate(op, a, b) {
+  switch(op) {
     case '+':
-      result = add(num1, num2)
+      result = add(a, b)
       break
     case '-':
-      result = subtract(num1, num2)
+      result = subtract(a, b)
       break
     case '*':
-      result = multiply(num1, num2)
+      result = multiply(a, b)
       break
     case '/':
-      if (num2 == 0) {
+      if (b === 0) {
         result = 'you can\'t divide by zero, nerd';
-      } else if (num2 != 0) {
-        result = divide(num1, num2)
+      } else if (b != 0) {
+        result = divide(a, b)
       }
+      break
+    case '^':
+      result = exponent(a, b)
       break
   }
   if (typeof(result) == 'number') {
-    displayScreen.innerHTML = result.toFixed(3);
+    updateScreenVal(parseFloat(result.toFixed(3)));
+    num1 = result
+    num2 = ''
+    currentNum = 1
+    operator = null
   } else {
-    displayScreen.innerHTML = result;
+    updateScreenVal(result);
   }
   
 }
 
 function switchOperator(op) {
-  operator = op
-  currentNum = 2
+  if (!operator) {
+    operator = op
+    currentNum = 2
+  } else if (operator) {
+    operate(operator, num1, num2)
+    num1 = result
+    num2 = ''
+    currentNum = 2
+    operator = op
+  }
 }
 
 function numberInput(num) {
-  if (currentNum === 1) {
+  if (currentNum === 1 && num1 != result) {
     num1 += num
-    displayScreen.innerHTML = num1
+    updateScreenVal(num1)
   } else if (currentNum === 2) {
     num2 += num
-    displayScreen.innerHTML = num2
+    updateScreenVal(num2)
+  }
+}
+
+function decimalInput(num) {
+  if (!/\./.test(num)) {
+    num += '.'
+    updateScreenVal(num)
+  }
+  return num
+}
+
+function clearScreen(aC) {
+  if (aC) {
+    operator = null
+    num1 = ''
+    num2 = ''
+    currentNum = 1
+    updateScreenVal()
+  } else if (!aC && num1 != result) {
+    if (currentNum === 1) {
+      num1 = num1.slice(0, -1)
+      updateScreenVal(num1)
+    } else if (displayScreen.innerHTML == num2) {
+      num2 = num2.slice(0, -1)
+      updateScreenVal(num2)
+    }
+  }
+}
+
+function updateScreenVal(val = '') {
+  displayScreen.innerHTML = val
+  if (displayScreen.innerHTML === '') {
+    displayScreen.innerHTML = 0
   }
 }
